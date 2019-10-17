93776bb9a62e2009126c9ea7f56e16d0c08528a7
Logan Keenan <d.logankeenan@gmail.com>
2019-10-17 16:28:54 -0500

    add integration test settings

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -11,3 +11,27 @@ class ActiveSupport::TestCase
 
   # Add more helper methods to be used by all tests here...
 end
+
+require 'capybara/rails'
+require 'capybara/minitest'
+
+class ActionDispatch::IntegrationTest
+  # Make the Capybara DSL available in all integration tests
+  include Capybara::DSL
+  # Make `assert_*` methods behave like Minitest assertions
+  include Capybara::Minitest::Assertions
+
+
+  def setup
+    Capybara.default_driver = :selenium_chrome_headless
+    Capybara.default_max_wait_time = 5
+  end
+
+  # Reset sessions and driver between tests
+  # Use super wherever this method is redefined in your individual test classes
+  def teardown
+    Capybara.reset_sessions!
+    Capybara.use_default_driver
+    Capybara.default_max_wait_time = 2
+  end
+end
